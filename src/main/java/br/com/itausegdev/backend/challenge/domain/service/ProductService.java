3ffbd6ffcf8ce00ccfc0068ceb9693f19e57a4a7
abraz <alcidesbraz@hotmail.com>
2023-12-26 22:52:43 -0300

    refatorando

--- src/main/java/br/com/itausegdev/backend/challenge/domain/service/ProductService.java
+++ src/main/java/br/com/itausegdev/backend/challenge/domain/service/ProductService.java
@@ -7,12 +7,8 @@ import br.com.itausegdev.backend.challenge.domain.builder.ProductBuilder;
 import java.util.List;
 
 public interface ProductService {
-
     ProductBuilder getProduct(String id);
-
     List<ProductResponse> getProducts();
-
     void createProduct(ProductRequest req);
-
     void updateProduct(ProductRequest request);
 }
