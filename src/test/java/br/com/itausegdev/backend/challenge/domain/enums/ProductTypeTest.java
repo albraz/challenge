a7582a6e32281eb202bba6c8b3e70dc4a0ed7c4a
abraz <alcidesbraz@hotmail.com>
2023-12-28 19:50:51 -0300

    clean code

--- src/test/java/br/com/itausegdev/backend/challenge/domain/enums/ProductTypeTest.java
+++ src/test/java/br/com/itausegdev/backend/challenge/domain/enums/ProductTypeTest.java
@@ -13,10 +13,15 @@ public class ProductTypeTest {
 
     @Test
     void shouldGetFeeValue() {
-        assertThat(ProductType.VIDA.getFee(new BigDecimal(100.00))).isEqualTo(new BigDecimal("103.2"));
-        assertThat(ProductType.AUTO.getFee(new BigDecimal(50.00))).isEqualTo(new BigDecimal("55.25"));
-        assertThat(ProductType.VIAGEM.getFee(new BigDecimal(100.00))).isEqualTo(new BigDecimal("107"));
-        assertThat(ProductType.RESIDENCIAL.getFee(new BigDecimal(100.00))).isEqualTo(new BigDecimal("107"));
-        assertThat(ProductType.PATRIMONIAL.getFee(new BigDecimal(100.00))).isEqualTo(new BigDecimal("108"));
+        assertThat(ProductType.VIDA.getFee(new BigDecimal("100.00")))
+                .isEqualTo(new BigDecimal("103.2"));
+        assertThat(ProductType.AUTO.getFee(new BigDecimal("50.00")))
+                .isEqualTo(new BigDecimal("55.25"));
+        assertThat(ProductType.VIAGEM.getFee(new BigDecimal("100.00")))
+                .isEqualTo(new BigDecimal("107"));
+        assertThat(ProductType.RESIDENCIAL.getFee(new BigDecimal("100.00")))
+                .isEqualTo(new BigDecimal("107"));
+        assertThat(ProductType.PATRIMONIAL.getFee(new BigDecimal("100.00")))
+                .isEqualTo(new BigDecimal("108"));
     }
 }
